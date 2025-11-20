bb5e60a5a985b7f796cae54541a837d5127d482e
FAOS <pompatcharin@gmail.com>
2025-11-20 19:39:52 +0700

    jason name change

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -6,7 +6,7 @@ import type { NextRequest } from 'next/server'
 import { createClient } from '@/utils/supabase/server'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-09-30.clover',
+  apiVersion: '2025-10-29.clover',
 })
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!
